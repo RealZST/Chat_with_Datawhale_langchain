ea5456f6354978e7ae54cc9ef34772e898c86d68
RealZST <zst9626@126.com>
2025-02-21 11:52:26 +0200

    update

--- qa_chain/model_to_llm.py
+++ qa_chain/model_to_llm.py
@@ -7,12 +7,15 @@ from langchain.chat_models import ChatOpenAI
 from llm.call_llm import parse_llm_api_key
 
 
-def model_to_llm(model:str=None, temperature:float=0.0, appid:str=None, api_key:str=None,Spark_api_secret:str=None,Wenxin_secret_key:str=None):
+def model_to_llm(model:str=None, temperature:float=0.0, appid:str=None, api_key:str=None, Spark_api_secret:str=None,Wenxin_secret_key:str=None):
         """
-        星火：model,temperature,appid,api_key,api_secret
-        百度问心：model,temperature,api_key,api_secret
-        智谱：model,temperature,api_key
-        OpenAI：model,temperature,api_key
+        Returns a wrapped LLM instance based on the model parameter.
+        
+        Supported models:
+        - ChatOpenAI: model,temperature,api_key
+        - Wenxin_LLM: model,temperature,api_key,api_secret
+        - Spark_LLM: model,temperature,appid,api_key,api_secret
+        - ZhipuAILLM: model,temperature,api_key
         """
         if model in ["gpt-3.5-turbo", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-0613", "gpt-4", "gpt-4-32k"]:
             if api_key == None:
